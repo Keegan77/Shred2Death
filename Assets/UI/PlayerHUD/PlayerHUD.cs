b3278ba45c1bb5198c6be0a748b3749920938dd7
AlexMullin <AlexMullin1@gmail.com>
2024-03-18 12:54:19 -0400

    Public widget variable
    my bad lol

--- Assets/UI/PlayerHUD/PlayerHUD.cs
+++ Assets/UI/PlayerHUD/PlayerHUD.cs
@@ -5,10 +5,5 @@ using UnityEngine;
 public class PlayerHUD : MonoBehaviour
 {
     [Tooltip("See the tooltips on the StatsWidget object to see how to use the widget.")]
-    StatsWidget stats;
-
-    private void Awake()
-    {
-        stats = transform.Find("StatsWidget").GetComponent<StatsWidget>();
-    }
+    public StatsWidget stats;
 }
