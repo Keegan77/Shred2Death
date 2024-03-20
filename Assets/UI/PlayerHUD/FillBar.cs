124060b8f29cae5946357c135208058e583503cb
Keegan <57238792+Keegan77@users.noreply.github.com>
2024-03-20 11:29:35 -0400

    fixed build issues

--- Assets/UI/PlayerHUD/FillBar.cs
+++ Assets/UI/PlayerHUD/FillBar.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class FillBar : MonoBehaviour
 {
