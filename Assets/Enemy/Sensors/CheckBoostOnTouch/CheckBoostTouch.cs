72669425c064f4e93616be5580da2222aefddf00
AMullin <AlexMullin1@gmail.com>
2024-04-01 15:37:21 -0400

    Started Boost Sensor

---
+++ Assets/Enemy/Sensors/CheckBoostOnTouch/CheckBoostTouch.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+/// <summary>
+/// Activates when the player enters the trigger while boosting.
+/// </summary>
+public class CheckBoostTouch : Sensor
+{
+    private void OnTriggerEnter (Collider other)
+    {
+        //if ( other.CompareTag ("Player") && Enemy.playerReference.playerbase.movement)
+        //{
+
+        //}
+    }
+}
