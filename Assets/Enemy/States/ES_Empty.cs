7af5f721092c825adf628d4762948a9a6d0c3b21
AlexMullin <AlexMullin1@gmail.com>
2024-04-02 10:14:57 -0400

    Enemy Null reference fix and HUD fix
    Hud shows up again now and enemies don't spam null references any more

--- Assets/Enemy/States/ES_Empty.cs
+++ Assets/Enemy/States/ES_Empty.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class ES_Empty : Enemy_State
 {
+    private void Awake ()
+    {
+        e = transform.parent.GetComponent<Enemy>();
+    }
 
+    public override void AIUpdate ()
+    {
+
+    }
 }
