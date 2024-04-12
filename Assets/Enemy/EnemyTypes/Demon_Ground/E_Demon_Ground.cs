cf8a8bb0ed87e2a2b7cf6aeb48a0ecdb4003db01
AlexMullin <AlexMullin1@gmail.com>
2024-04-12 13:32:50 -0400

    Sensor Layer and Dodge Fix
    Sensors are now on their own layer
    Dodging now goes in the right direction for all directions the player is going

--- Assets/Enemy/EnemyTypes/Demon_Ground/E_Demon_Ground.cs
+++ Assets/Enemy/EnemyTypes/Demon_Ground/E_Demon_Ground.cs
@@ -33,8 +33,8 @@ public class E_Demon_Ground : Enemy
         //rb.isKinematic = false;
         //rb.useGravity = true;
 
-        Debug.Log ("Checking agent settings");
-        Debug.Log (agent.agentTypeID);
+        //Debug.Log ("Checking agent settings");
+        //Debug.Log (agent.agentTypeID);
         if (agentSettings != null)
         {
             for (int i = 0; i < agentSettings.Length; i++)
