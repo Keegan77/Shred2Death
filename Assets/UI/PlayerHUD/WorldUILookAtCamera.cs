71cb5211d6c95c674869c6d44b837959a4156f1e
Keegan <57238792+Keegan77@users.noreply.github.com>
2024-04-17 12:11:05 -0400

    Created boilerplate for aligning a world space UI element to the camera's look direction

---
+++ Assets/UI/PlayerHUD/WorldUILookAtCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldUILookAtCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        //transform.LookAt(mainCamera.transform);
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+    }
+}
